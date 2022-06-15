acd924b0be13c3e9dced037593a70277320d61d7
Mitchell Stowman <92927307+StowCode@users.noreply.github.com>
2022-06-14 20:37:45 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navigation from './Components/navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cherokee Orchestra</h1>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Navigation() {
+    return (
+      <div className="App">
+        <h1>Navigation</h1>
+      </div>
+    );
+  }
+  
+  export default Navigation;
+  
