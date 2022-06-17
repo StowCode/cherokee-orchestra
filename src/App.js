e81981dcbb6f208eb0994e5f87a0d05438240485
Mitchell Stowman <92927307+StowCode@users.noreply.github.com>
2022-06-17 18:51:58 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navigation from './Components/navigation'
 function App() {
   return (
     <div className="App">
-      <h1>Cherokee Orchestra</h1>
       <Navigation />
     </div>
   );
--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -1,9 +1,12 @@
+import './Navigation.css';
 import React from 'react';
+import logo from './Resources/ChorchIconFinal.png';
 
 function Navigation() {
     return (
-      <div className="App">
-        <ul id="navbar">
+      <div className="Navigation">
+        <img src={logo} alt='Cherokee Orchestra Icon' id='nav_icon' />
+        <ul id="navbar_links">
             <li className="main-link">About Us</li>
             <li className="main-link">2022-23 Sechedule</li>
             <li className="main-link">Chamber Strings</li>
